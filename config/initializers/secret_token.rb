3053b172039630ee22c4221a41865445bfe1cc3b
Igor Okulist <okigan@yahoo.com>
2011-02-07 21:33:46 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tmiw3::Application.config.secret_token = 'af9b61349505e738323460c9f5723e2a6508614594d1c06600bb523d0359cde7155d718fa68d6fb2a11bf077e2f704c11cecb015e15ed6cf13e96cd59a91e85b'
